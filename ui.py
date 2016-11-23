3fb8f2c7db51562f2f4fb742915f1a0d411506e0
BiroMarton <rezoner9@gmail.com>
2016-11-23 11:16:17 +0100

    wrote def print_menu and def print_result

--- ui.py
+++ ui.py
@@ -23,11 +23,8 @@ def print_table(table, title_list):
 # @result: string or list or dictionary - result of the special function
 # @label: string - label of the result
 def print_result(result, label):
-
-    # your code
-
-    pass
-
+    print(label + ": ")
+    print(result)
 
 # This function needs to generate outputs like this:
 # Main menu:
@@ -42,11 +39,15 @@ def print_result(result, label):
 # @title: string - title of the menu
 # @list_options: list of strings - the options in the menu
 # @exit_message: string - the last option with (0) (example: "Back to main menu")
+
+
 def print_menu(title, list_options, exit_message):
-
-    # your code
-
-    pass
+    print(title)
+    num = 1
+    for i in list_options:
+        print("(" + str(num) + ") " + i)
+        num += 1
+    print("(0) " + exit_message)
 
 
 # This function gets a list of inputs from the user by the terminal
